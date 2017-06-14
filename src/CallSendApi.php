49965665aa6d84ec1c5877995e3baba8f7d9b069
NiltonMorais <nilton_even@hotmail.com>
2017-06-14 13:26:15 -0300

    teste de integraçao e classe de requisiçao http

---
+++ src/CallSendApi.php
@@ -0,0 +1,30 @@
+<?php
+namespace CodeBot;
+
+use GuzzleHttp\Client;
+
+class CallSendApi{
+    const URL = 'https://graph.facebook.com/v2.6/me/messages';
+    /**
+     * @var string
+     */
+    private $pageAccessToken;
+
+    public function __construct(string $pageAccessToken)
+    {
+        $this->pageAccessToken = $pageAccessToken;
+    }
+
+    public function make(array $message, string $url = null,$method = 'POST'):string
+    {
+        $client = new Client;
+        $url = $url ?? CallSendApi::URL;
+
+        $response = $client->request($method,$url, [
+           'json' => $message,
+            'query' => ['access_token'=>$this->pageAccessToken]
+        ]);
+
+        return (string)$response->getBody();
+    }
+}
---
+++ tests/integration/CallSendApiTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace CodeBot;
+
+use CodeBot\Message\Text;
+use PHPUnit\Framework\TestCase;
+
+class CallSendApiTest extends TestCase{
+    /**
+     * @expectedException \GuzzleHttp\Exception\ClientException
+     */
+    public function testMakeRequest()
+    {
+        $message = (new Text(1))->message('Olá');
+        (new CallSendApi('123asd123'))->make($message);
+    }
+}
