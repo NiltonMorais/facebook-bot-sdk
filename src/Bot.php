0cf97a3ca0ee47a0a2416fa9a90c58cd160b971c
Nilton Morais <nilton_even@hotmail.com>
2018-02-16 17:46:47 -0300

    corrigindo template do Bot

--- src/Bot.php
+++ src/Bot.php
@@ -40,7 +40,7 @@ class Bot
         }
 
         foreach($elements as $element){
-            $type->add($elements);
+            $type->add($element);
         }
 
         $message = $type->message($message);
